43e8001ce62c75e8a6a892b569acbd1a813b70bc
Alex Madaras <alexandru.madaras00@e-uvt.ro>
2022-01-02 04:05:33 +0200

    feature-backend-setup
    -Initialized Firebase Login

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,12 @@
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-val uname=oninput()
-val password=oninput()
+const auth = getAuth();
+signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        const user = userCredential.user;
+        caches("Logged in"+user);
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    });
