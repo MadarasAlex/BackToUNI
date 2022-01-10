540f9837e49721ad2f2e6c287f61ecf597d1fbb3
Alex Madaras <alexandru.madaras00@e-uvt.ro>
2022-01-10 10:16:59 +0200

    update-backend-login-implementation
    -Started the implementation of the login part

--- scripts/login.js
+++ scripts/login.js
@@ -1,12 +1,28 @@
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import {getAuth, signInWithEmailAndPassword} from "firebase/auth";
 
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("submit-button")
 const auth = getAuth();
-signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-        const user = userCredential.user;
-        caches("Logged in"+user);
-    })
-    .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-    });
+loginButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+    if (username === "user" && password === "web_dev") {
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                caches("Logged in" + user);
+                window.location.href='./overview.html';
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+            });
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
+
+
